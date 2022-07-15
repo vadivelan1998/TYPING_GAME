c50bfed40c8cf9f0e64c735a7802372f7e0aa765
vadivelan1998 <vadivelan1998@gmail.com>
2022-07-16 02:12:55 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,37 +1,63 @@
-import { Alphabet } from "../Alphabet/Alphabet"
- import { useState } from "react";
+import { Alphabet } from "../AlphabetCard/AlphabetCard";
+//import { useContext, useState } from "react";
 import { Timer } from "../Timer/Timer";
-export const Home=()=>{
-    const [count,setCount]=useState(0)
-    console.log(count)
-const [onTimer, setOnTimer] = useState(false);
-const [onAlpha,setOnAlpha]=useState(false)
-// if(count==20)
-// {
-//     setOnTimer(false)
-// }
-    return (
-      <div className="home">
-        <h1>Type The Alphabet</h1>
-        <h3>
-          Typing game to see how fast type. The timer starts when you do :)
-        </h3>
+//import { Context } from "../Contexts/Context";
+import { Input } from "../Input/Input";
 
-        <br />
-        <Alphabet onAlpha={onAlpha} setOnAlpha={setOnAlpha} />
+export const Home = () => {
+//   const { setResult, setTimer, setOnTimer, letter, randomAlphabet } =
+//     useContext(Context);
+//   const [count, setCount] = useState(0);
+//   console.log(count);
+//   // const [onTimer, setOnTimer] = useState(false
 
-        <Timer onTimer={onTimer} />
-        <input
-          onKeyPress={() => {
-            setCount((prev)=>prev+1)
-             setOnTimer(true);
-             setOnAlpha(true)
-          }}
-          className="input"
-          type="text"
-          placeholder="Type Here"
-        />
-        <button className="reset">Reset</button>
-      </div>
-    );
-}
+  //const [key, setKey] = useState("");
+//   function handleInput(e) {
+//     if (e.key !== letter) {
+//       setTimer((prev) => prev + 500);
+//     }
+//     if (count <= 20) {
+//       console.log(e.key);
+//       setCount((prev) => prev + 1);
+//       setOnTimer(true);
+//       randomAlphabet();
+//       setKey(e.key);
+//     } else {
+//       setOnTimer(false);
+//       setResult(true);
+//       document.querySelector(".input").disabled = true;
+//     }
+//   }
+
+  return (
+    <div className="home">
+      <h1>Type The Alphabet</h1>
+      <h3>Typing game to see how fast type. The timer starts when you do :)</h3>
+
+      <br />
+      <Alphabet/>
+
+      <Timer />
+      {/* <input
+        onKeyPress={handleInput}
+        className="input"
+        type="text"
+        placeholder="Type Here"
+      />
+      <button
+        onClick={() => {
+          setTimer(0);
+          setOnTimer(false);
+          setCount(0);
+          document.querySelector(".input").value = "";
+          document.querySelector(".input").disabled = false;
+          setResult(false)
+        }}
+        className="reset"
+      >
+        Reset
+      </button> */}
+      <Input/>
+    </div>
+  );
+};
