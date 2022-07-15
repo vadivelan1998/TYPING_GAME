b1617bd05dc401cf13e14dee779a40d0c48c84ab
vadivelan1998 <vadivelan1998@gmail.com>
2022-07-16 02:13:07 +0530

    Update Timer.jsx

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,19 +1,8 @@
-import { useState,useEffect } from "react";
-
-export const Timer=({onTimer})=>{
-     const [timer, setTimer] = useState(0);
-     
-     useEffect(() => {
-       let interval = null;
-       if (onTimer) {
-         interval = setInterval(() => {
-           setTimer((prev) => prev + 10);
-         }, 10);
-       } else {
-         clearInterval(interval);
-       }
-       return () => clearInterval(interval);
-     }, [onTimer]);
+import {  useContext } from "react";
+import { Context } from "../Contexts/Context";
+export const Timer=()=>{
+    const { timer } = useContext(Context);
+  
     return (
       <div>
         <h2>
