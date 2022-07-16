cbd73419abaefe0d98a397efa8c609c6395d2f18
vadivelan1998 <vadivelan1998@gmail.com>
2022-07-16 12:14:04 +0530

    Update AlphabetCard.jsx

--- src/components/AlphabetCard/AlphabetCard.jsx
+++ src/components/AlphabetCard/AlphabetCard.jsx
@@ -1,19 +1,13 @@
 import { useContext } from "react";
 import { Context } from "../Contexts/Context";
 export const AlphabetCard = () => {
-  const { timer,mybesttime,letter, result } = useContext(Context);
+  const { letter } = useContext(Context);
 
   return (
     <div className="alphabet">
-      {result ? (
-        timer < mybesttime ? (
-          <h3>" SUCCESS ! "</h3>
-        ) : (
-          <h3>" FAILURE ! "</h3>
-        )
-      ) : (
+      
         <h1>{letter}</h1>
-      )}
+      
     </div>
   );
 };
